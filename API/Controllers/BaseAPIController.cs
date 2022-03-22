4aea4a5c14c3fe7c0cc7b78aea49ac4bf02e04cb
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-03-22 20:24:31 +0200

    Deployment config

--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,4 +1,6 @@
 // using API.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,6 +8,8 @@ namespace API.Controllers
     // [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
+    [EnableCors("EnableCors")]
     public class BaseApiController : ControllerBase
     {
     }
