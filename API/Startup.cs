bfb59eaee4d8df2ff9d9aa414ce626606e9eeb6a
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-03-22 21:00:23 +0200

    Deployment config

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,23 +1,8 @@
-using System.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using API.Extensions;
 using API.Middleware;
 
@@ -44,14 +29,12 @@ namespace API
           //      c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
           //  });
 
-            services.AddCors(options =>
-            options.AddPolicy("EnableCors", builder =>
-                builder.SetIsOriginAllowed(origin => true).AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+            services.AddCors();
 
+            services.AddIdentityServices(_config);
 
             services.AddControllers();
 
-            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,13 +53,12 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors("EnableCors");
             
-            // app.UseCors(policy => 
-            // policy
-            // .AllowAnyHeader()
-            // .AllowAnyMethod()
-            // .WithOrigins("https://drop-shipping-client.herokuapp.com"));
+            app.UseCors(policy => 
+            policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://drop-shipping-client.herokuapp.com"));
 
             app.UseAuthentication();
             
