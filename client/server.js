a4114f9f94edb37b72f14cbbf257625014eb5228
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-03-22 19:40:40 +0200

    Deployment config

--- client/server.js
+++ client/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/client'));
+app.use(express.static(__dirname + '/dist/client/src'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/client/index.html'));});
+'/dist/client/src/index.html'));});
 app.listen(process.env.PORT || 8080);
