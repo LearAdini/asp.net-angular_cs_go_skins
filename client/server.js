ecfc1f5d624e20ca455ec9c92088f96ce6d39030
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-03-24 13:09:49 +0200

    Deployment config

--- client/server.js
+++ client/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
