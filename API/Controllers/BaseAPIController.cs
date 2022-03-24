d7164f7e5637e53b8c1dad40b38f30591b87f9f3
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-03-24 14:27:53 +0200

    Deployment config

--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,7 +1,9 @@
+using System.Web.Http.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [EnableCors(origins: "http://https://drop-shipping-client.herokuapp.com", headers: "*", methods: "*")]
     [ApiController]
     [Route("api/[controller]")]
 
