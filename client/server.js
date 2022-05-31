db6ce15ee0a26a56eaf9801e9dfeb91559f3f9c6
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-05-31 22:35:34 +0300

    Major Changes

--- client/server.js
+++ client/server.js
@@ -6,7 +6,17 @@ const app = express();
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //  next();
 // });
-app.use(cors()); // <---- use cors middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// app.use(cors());
 
 // app.use(bodyParser.urlencoded({extended: false}));
 // app.use(bodyParser.json());
