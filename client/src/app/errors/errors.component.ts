fc1e4c952e4a89aef2e33dbaaf90508e34abff75
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-05-31 20:51:55 +0300

    Major Changes

--- client/src/app/errors/errors.component.ts
+++ client/src/app/errors/errors.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorsComponent implements OnInit {
 
-  baseURL = "https://localhost:5001/api/";
+  baseURL = "http://localhost:5001/api/";
   validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api/'
+  apiUrl: 'http://localhost:5001/api/'
 };
