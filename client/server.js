7c7db452d6a9d9d71342e02632cdcab4da5504a6
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-05-31 22:41:56 +0300

    Major Changes

--- client/server.js
+++ client/server.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//  next();
-// });
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
 });
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-// app.use(cors());
-
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://csgo-skins-angular.herokuapp.com/api/'
+  apiUrl: 'https://csgo-skins-angular.herokuapp.com/api'
 };
