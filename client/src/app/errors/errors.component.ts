fb8a459a13a8701e67cece7315770abe4f4855cd
LearAdini <80118008+BeatrixKiddo1@users.noreply.github.com>
2022-05-31 21:45:48 +0300

    Major Changes

--- client/src/app/errors/errors.component.ts
+++ client/src/app/errors/errors.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorsComponent implements OnInit {
 
-  baseURL = "https://localhost:5001/api/";
+  baseURL = "https://csgo-skins-angular.herokuapp.com/api/";
   validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
